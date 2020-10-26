43e087f9d0d208970e90090a80c2b114f8b0fa47
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2020-10-26 13:22:03 +0000

    Spell Check & PEP8 Fixes

--- cli.py
+++ cli.py
@@ -1,6 +1,8 @@
 import argparse
 import json
 import os
+import sys
+
 from excel.load import get_dict_from_excel
 from excel.validate import validate_dict_from_excel
 
@@ -18,10 +20,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse, Validate and Submit excel files to EBI Resources')
     parser.add_argument('file_path', type=str, help='path of excel file to load')
     parser.add_argument('--biosamples', action='store_true', help='Submit to BioSamples, requires environment variables AAP_USERNAME and AAP_PASSWORD')
-    
-    parser.add_argument('--biosamples_url', type=str, default='https://www.ebi.ac.uk/biosamples', help='Overridde the default URL for BioSamples API.')
-    parser.add_argument('--aap_url', type=str, default='https://api.aai.ebi.ac.uk', help='Overridde the default URL for AAP API.')
-    
+
+    parser.add_argument('--biosamples_url', type=str, default='https://www.ebi.ac.uk/biosamples', help='Override the default URL for BioSamples API.')
+    parser.add_argument('--aap_url', type=str, default='https://api.aai.ebi.ac.uk', help='Override the default URL for AAP API.')
+
     args = vars(parser.parse_args())
     excel_file_path = args['file_path']
 
@@ -40,27 +42,27 @@ if __name__ == '__main__':
 
     if args['biosamples']:
         if not data:
-            print ('No Data to Submit to BioSamples')
-            exit(2)
+            print('No Data to Submit to BioSamples')
+            sys.exit(2)
 
         # ToDo: if issues: Ask user if they want to proceed with detected issues.
         # This may have to wait until we can classify issues into Errors (that will block submission), Warnings and Info
 
         biosamples_url = args['biosamples_url']
         aap_url = args['aap_url']
-        
+
         if 'AAP_USERNAME' in os.environ:
             aap_username = os.environ['AAP_USERNAME']
         else:
             print('No AAP_USERNAME detected in os environment variables.')
-            exit(2)
-        
+            sys.exit(2)
+
         if 'AAP_PASSWORD' in os.environ:
             aap_password = os.environ['AAP_PASSWORD']
         else:
             print('No AAP_PASSWORD detected in os environment variables.')
-            exit(2)
-        
+            sys.exit(2)
+
         print(f'Attempting to Submit to BioSamples: {biosamples_url}, AAP: {aap_url}')
-    
-    exit(0)
+
+    sys.exit(0)
