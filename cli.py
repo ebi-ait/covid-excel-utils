f29b10d2e8adb0e905ca487ffe9962d35e73a7cf
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2020-10-26 13:12:28 +0000

    BioSamples CLI arguments

--- cli.py
+++ cli.py
@@ -15,8 +15,13 @@ def write_dict(file_path, data_dict):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='')
+    parser = argparse.ArgumentParser(description='Parse, Validate and Submit excel files to EBI Resources')
     parser.add_argument('file_path', type=str, help='path of excel file to load')
+    parser.add_argument('--biosamples', action='store_true', help='Submit to BioSamples, requires environment variables AAP_USERNAME and AAP_PASSWORD')
+    
+    parser.add_argument('--biosamples_url', type=str, default='https://www.ebi.ac.uk/biosamples', help='Overridde the default URL for BioSamples API.')
+    parser.add_argument('--aap_url', type=str, default='https://api.aai.ebi.ac.uk', help='Overridde the default URL for AAP API.')
+    
     args = vars(parser.parse_args())
     excel_file_path = args['file_path']
 
@@ -26,7 +31,36 @@ if __name__ == '__main__':
     if data:
         json_file_path = os.path.splitext(excel_file_path)[0] + '.json'
         write_dict(json_file_path, data)
+        print(f'Data written to: {json_file_path}')
 
     if issues:
         issues_file_path = os.path.splitext(excel_file_path)[0] + '_issues.json'
         write_dict(issues_file_path, issues)
+        print(f'Issues written to: {issues_file_path}')
+
+    if args['biosamples']:
+        if not data:
+            print ('No Data to Submit to BioSamples')
+            exit(2)
+
+        # ToDo: if issues: Ask user if they want to proceed with detected issues.
+        # This may have to wait until we can classify issues into Errors (that will block submission), Warnings and Info
+
+        biosamples_url = args['biosamples_url']
+        aap_url = args['aap_url']
+        
+        if 'AAP_USERNAME' in os.environ:
+            aap_username = os.environ['AAP_USERNAME']
+        else:
+            print('No AAP_USERNAME detected in os environment variables.')
+            exit(2)
+        
+        if 'AAP_PASSWORD' in os.environ:
+            aap_password = os.environ['AAP_PASSWORD']
+        else:
+            print('No AAP_PASSWORD detected in os environment variables.')
+            exit(2)
+        
+        print(f'Attempting to Submit to BioSamples: {biosamples_url}, AAP: {aap_url}')
+    
+    exit(0)
