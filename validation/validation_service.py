07f1fc89cee1434aebca94c6fe5169de050dd3d6
Karoly Erdos <karoly@ebi.ac.uk>
2020-11-04 11:39:43 +0000

    Add back lower case conversion
    also renamed the schema and only load them once

--- validation/validation_service.py
+++ validation/validation_service.py
@@ -4,7 +4,7 @@ import json
 import os
 
 from .not_known_entity_exception import NotKnownEntityException
-from .validation_state import ValidationState
+
 
 def translate_json_schema_error_to_human(object_name: str, schema_errors: dict):
     translated_messages = []
@@ -14,15 +14,18 @@ def translate_json_schema_error_to_human(object_name: str, schema_errors: dict):
             translated_messages.append(f'Error: {object_name}{path} {error}')
     return translated_messages
 
+
 class ValidationService:
-    SCHEMA_FILENAME_PREFIX = "covid_data_uploader-"
     SCHEMA_FILENAME_EXTENSION = ".json"
     SCHEMA_FILES_FOLDER = "../json-schema/"
     ENTITY_MAPPING_FILE = "../config/schema_by_entity_mapping.json"
+    ENTITY_TYPES = ['study', 'sample', 'run_experiment', 'isolate_genome_assembly_information']
+    CURRENT_FOLDER = os.path.dirname(__file__)
+    schema_by_type = {}
 
     def __init__(self, validator_url):
         self.validator_url = validator_url
-        self.current_folder = os.path.dirname(__file__)
+        ValidationService.__load_schema_files()
 
     def validate_data(self, data):
         issues = {}
@@ -30,11 +33,9 @@ class ValidationService:
             for entity_type, entity in entities.items():
                 if entity_type == 'row':
                     continue
-                # ToDo: These schema files never change and yet we load them from disk during every row of the excel spreadsheet!
-                schema_file_name = f'{self.SCHEMA_FILENAME_PREFIX}{self.get_schema_by_entity_type(entity_type)}{self.SCHEMA_FILENAME_EXTENSION}'
-                with open(os.path.join(self.current_folder, f'{self.SCHEMA_FILES_FOLDER}{schema_file_name}')) as schema_file:
-                    schema = json.load(schema_file)
-                
+
+                schema = self.schema_by_type[entity_type]
+
                 validation_result = self.validate_by_schema(schema, entity).json()
                 human_errors = translate_json_schema_error_to_human(entity_type, validation_result)
                 if human_errors:
@@ -49,8 +50,28 @@ class ValidationService:
 
         return validation_response
 
-    def get_schema_by_entity_type(self, entity_type):
-        with open(os.path.join(self.current_folder, f"{self.ENTITY_MAPPING_FILE}")) as schema_config_file:
+    @staticmethod
+    def __create_validator_payload(schema, object_to_validate):
+        #  TODO is there a better way to make it lowercase?
+        object_to_validate = json.loads(json.dumps(object_to_validate).lower())
+        return {
+            "schema": schema,
+            "object": object_to_validate
+        }
+
+    @staticmethod
+    def __load_schema_files():
+        for entity_type in ValidationService.ENTITY_TYPES:
+            schema_file_name = \
+                f'{ValidationService.get_schema_by_entity_type(entity_type)}{ValidationService.SCHEMA_FILENAME_EXTENSION}'
+            with open(os.path.join(ValidationService.CURRENT_FOLDER,
+                                   f'{ValidationService.SCHEMA_FILES_FOLDER}{schema_file_name}')) as schema_file:
+                ValidationService.schema_by_type[entity_type] = json.load(schema_file)
+
+    @staticmethod
+    def get_schema_by_entity_type(entity_type):
+        with open(os.path.join(ValidationService.CURRENT_FOLDER,
+                               f"{ValidationService.ENTITY_MAPPING_FILE}")) as schema_config_file:
             schema_config = json.load(schema_config_file)
 
         try:
@@ -59,11 +80,4 @@ class ValidationService:
             raise NotKnownEntityException(entity_type)
 
         return schema_name
-
-    @staticmethod
-    def __create_validator_payload(schema, object_to_validate):
-        return {
-            "schema": schema,
-            "object": object_to_validate
-        }
 
