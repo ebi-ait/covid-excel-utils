afb6d9dbb946360dfea566fa1594c7a7ee626e8d
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2020-11-17 12:34:55 +0000

    Make Docker Logging easier to parse.

--- validation/docker.py
+++ validation/docker.py
@@ -9,43 +9,36 @@ class DockerValidator(SchemaValidator):
     def __init__(self, image_name, validator_url, port=3020):
         self.__image_name = image_name
         self.__client = docker.from_env()
-        self.__clean_up(self.__client, image_name)
         self.container = self.__launch(self.__client, image_name, port)
         super().__init__(validator_url)
     
     def close(self):
+        self.container.reload()
         if self.container:
-            logging.info(f'Stop running {self.__image_name} container image.')
+            logging.info(f'Stop running container: {self.container.name}')
             self.container.stop()
-            logging.info(f'Remove {self.__image_name} container image.')
+            logging.info(f'Removing container image: {self.container.name}')
             self.container.remove()
             self.__client.close()
-
-    @staticmethod
-    def __clean_up(docker_client, image_name):
-        logging.info(f'Remove dangling container images with {image_name} name.')
-        for state in ['created', 'restarting', 'removing', 'paused', 'exited', 'dead']:
-            # This will remove the user's docker images, are we sure we want to do this?
-            containers_to_remove = docker_client.containers.list(filters={'status': f'{state}'})
-            for container_to_remove in containers_to_remove:
-                container_to_remove.reload()
-                container_to_remove.remove()
     
     @staticmethod
     def __launch(docker_client, image_name, port):
         if not docker_client.images.list(image_name):
-            logging.info(f'Pull {image_name} container image from DockerHub.')
+            logging.info(f'Pulling image: {image_name}')
             docker_client.images.pull(image_name)
+            logging.info(f'Pulled image from DockerHub: {image_name}')
         containers = docker_client.containers.list(filters={'ancestor': image_name})
         if containers:
+            logging.info(f'Attaching to existing container from image: {image_name}')
             container = containers[0]
         else:
-            logging.info(f'Run {image_name} container image locally.')
+            logging.info(f'Starting container from image: {image_name}')
             container = docker_client.containers.run(
                 image_name,
                 ports={f'{port}/tcp': port},
                 detach=True
             )
             time.sleep(5)
+        logging.info(f'Running Container: {container.name}')
         container.reload()
         return container
