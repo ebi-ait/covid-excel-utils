53505b9951991b06b531fc81548b7e5cd71fa708
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2021-02-18 12:35:18 +0000

    ENA Submission xml

--- conversion/ena/submission.py
+++ conversion/ena/submission.py
@@ -1,6 +1,8 @@
+from datetime import date
 from lxml import etree
 from xml.etree.ElementTree import Element
 
+from submission.entity import Entity
 from submission.submission import Submission
 from .project import EnaProjectConverter
 from .study import EnaStudyConverter
@@ -17,7 +19,7 @@ class EnaSubmissionConverter:
             'samples.xml': self.samples_file,
             'experiments.xml': self.experiments_file,
             'runs.xml': self.runs_file,
-            #  'submission.xml': self.submission_file
+            'submission.xml': self.submission_file
         }
 
     def convert(self, data: Submission) -> dict:
@@ -32,7 +34,6 @@ class EnaSubmissionConverter:
         projects_node = etree.XML('<PROJECT_SET />')
         for study in data.get_entities('study'):
             projects_node.append(project_converter.convert(study))
-        # Handle study.release_date
         return projects_node
 
     @staticmethod
@@ -41,7 +42,6 @@ class EnaSubmissionConverter:
         studies_node = etree.XML('<STUDY_SET />')
         for study in data.get_entities('study'):
             studies_node.append(study_converter.convert(study))
-        # Handle study.release_date
         return studies_node
 
     @staticmethod
@@ -81,3 +81,23 @@ class EnaSubmissionConverter:
         for run in data.get_entities('run_experiment'):
             runs_node.append(run_converter.convert_run(run, experiment=run))
         return runs_node
+    
+    @staticmethod
+    def submission_file(data: Submission) -> Element:
+        submission = etree.Element('SUBMISSION')
+        actions = etree.SubElement(submission, 'ACTIONS')
+        action_add = etree.SubElement(actions, 'ACTION')
+        etree.SubElement(action_add, 'ADD')
+        if 'study' in data.get_entity_types():
+            release_dates = set()
+            study: Entity
+            for study in data.get_entities('study'):
+                if 'release_date' in study.attributes:
+                    release_dates.add(study.attributes['release_date'])
+            if len(release_dates) > 0:
+                # Use first study release date, possible that file includes many 
+                release_date = date.fromisoformat(release_dates.pop())
+                action_hold = etree.SubElement(actions, 'ACTION')
+                hold = etree.SubElement(action_hold, 'HOLD')
+                hold.attrib['HoldUntilDate'] = release_date.isoformat()
+        return submission
